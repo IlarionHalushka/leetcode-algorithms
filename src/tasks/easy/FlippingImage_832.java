a7632d0dddd77b52a7a7d9880b76cdd13865c23a
Ilarion <hilarion.galushka@gmail.com>
2018-12-11 23:13:03 +0200

    added link and description to task№832: flipping image

--- src/tasks/easy/FlippingImage_832.java
+++ src/tasks/easy/FlippingImage_832.java
@@ -3,6 +3,17 @@ package tasks.easy;
 import java.io.IOException;
 
 class FlippingImage_832 {
+    // https://leetcode.com/problems/flipping-an-image/
+
+    // Given a binary matrix A, we want to flip the image horizontally,
+    // then invert it, and return the resulting image.
+    //
+    // To flip an image horizontally means that each row of the image is reversed.
+    //  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+    //
+    // To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+    // For example, inverting [0, 1, 1] results in [1, 0, 0].
+
     public static void main(String[] args) throws IOException {
         int[][] array = new int[][]{{1,0,1},{1,0,0},{0,0,1},{1,1,1},{0,1,0}};
 
