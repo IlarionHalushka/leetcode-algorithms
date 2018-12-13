7ad8bb6a4fa315e482c92b822b409f7d618c29d6
Ilarion <hilarion.galushka@gmail.com>
2018-12-13 22:26:42 +0200

    task binary gap

---
+++ src/tasks/easy/BinaryGap.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+class BinaryGap {
+    public static void main(String[] args) throws IOException {
+        int N = 12;
+        String binaryStr = Integer.toBinaryString(N);
+        System.out.println(binaryStr);
+        int maxDist = 0;
+        int dist = 0;
+
+        for (int i = 0; i < binaryStr.length() - 1; i++) {
+            if (binaryStr.charAt(i) == '1') {
+                while ((i + dist) < binaryStr.length() - 1 && binaryStr.charAt(i + dist) != '1') {
+                    dist++;
+                }
+
+                if (binaryStr.charAt(i + dist) != '1') {
+                    dist = 0;
+                }
+                maxDist = Math.max(maxDist, dist);
+            }
+        }
+
+        System.out.println(maxDist);
+    }
+
+}
