6f0105db0f6669c98253101bae13140672a30ed5
Ilarion <hilarion.galushka@gmail.com>
2018-12-15 18:17:18 +0200

    improved solution of task DI_String_Match_942 https://leetcode.com/problems/di-string-match/

--- src/tasks/easy/DI_String_Match_942.java
+++ src/tasks/easy/DI_String_Match_942.java
@@ -10,23 +10,20 @@ class DI_String_Match_942 {
     }
 
     private static int[] diStringMatch(String S) {
-        int[] result = new int[S.length() + 1];
+        int N = S.length();
+        int[] result = new int[N + 1];
         int lowest = 0;
-        int highest = S.length();
+        int highest = N;
 
-        for (int i = 0; i < S.length(); i++) {
+        for (int i = 0; i < N; i++) {
             if (S.charAt(i) == 'I') {
-                result[i] = lowest;
-                lowest++;
-            }
-
-            if (S.charAt(i) == 'D') {
-                result[i] = highest;
-                highest--;
+                result[i] = lowest++;
+            } else {
+                result[i] = highest--;
             }
         }
 
-        result[S.length()] = lowest;
+        result[N] = lowest;
 
         return result;
     }
