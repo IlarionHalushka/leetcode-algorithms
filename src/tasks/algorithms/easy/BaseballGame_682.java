e4ce783d134917523cf546dddc2de46804e041bf
Ilarion <hilarion.galushka@gmail.com>
2018-12-16 13:57:25 +0200

    improved solution of task BaseballGame_682 https://leetcode.com/problems/baseball-game/

--- src/tasks/algorithms/easy/BaseballGame_682.java
+++ src/tasks/algorithms/easy/BaseballGame_682.java
@@ -1,5 +1,7 @@
 package tasks.algorithms.easy;
 
+import java.util.ArrayList;
+
 class BaseballGame_682 {
     public static void main(String[] args) {
         System.out.println(calPoints(new String[]{"5","-2","4","C","D","9","+","+","C","D","5","0","2","C","+","D","D","+","C","1","5","12","D","C","+"}));
@@ -8,41 +10,31 @@ class BaseballGame_682 {
     }
 
     public static int calPoints(String[] ops) {
-        int last = 0;
-        int prelast = 0;
-        int sum = 0;
+        int N = ops.length;
+        ArrayList<Integer> points = new ArrayList<>();
+        points.add(0);
+        points.add(0);
 
-        for (int i = 0; i < ops.length; i++) {
+        for (int i = 0; i < N; i++) {
             if (ops[i].equals("C")) {
-                sum -= last;
-                last = Integer.MAX_VALUE;
+                points.remove(points.size() - 1);
 
             } else if (ops[i].equals("D")) {
-                if (last != Integer.MAX_VALUE) {
-                    last = last * 2;
-                    sum += last;
-                } else {
-                    last = prelast * 2;
-                    sum += last;
-                }
+                points.add(points.get(points.size() - 1) * 2);
 
 
             } else if (ops[i].equals("+")) {
-                if (last != Integer.MAX_VALUE) {
-                    sum += last + prelast;
-                    int tmp = last + prelast;
-                    prelast = last;
-                    last = tmp;
-
-                } else {
-                    sum += prelast;
-                }
+                points.add(points.get(points.size() - 1) + points.get(points.size() - 2));
 
             } else {
-                prelast = last;
-                last = Integer.parseInt(ops[i]);
-                sum += last;
+                points.add(Integer.parseInt(ops[i]));
+
             }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < points.size(); i++) {
+            sum += points.get(i);
         }
 
         return sum;
