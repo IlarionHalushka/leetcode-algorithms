0dc7c8873ecb2a08178b6a9ac4fb790ff7f4b0df
Ilarion <hilarion.galushka@gmail.com>
2018-12-24 20:14:43 +0200

    solve task MergeTwoBinaryTrees https://leetcode.com/problems/merge-two-binary-trees/

---
+++ src/tasks/algorithms/medium/MergeTwoBinaryTrees_617.js
@@ -0,0 +1,11 @@
+// my solution
+var mergeTrees = function(t1, t2) {
+    if ((t1 == null || t1.val == null) && (t2 == null || t2.val == null)) return null;
+    const root = new TreeNode((t1 ? t1.val : null) + (t2 ? t2.val : null));
+
+    root.left = mergeTrees((t1 ? t1.left : null), (t2 ? t2.left : null));
+    root.right = mergeTrees((t1 ? t1.right : null), (t2 ? t2.right : null));
+
+    return root;
+};
+
