00bd519047a7ed284b85c9a689fa02a95f0d2d87
Ilarion <hilarion.galushka@gmail.com>
2019-01-06 18:58:12 +0200

    improve solution of task IntersectionOfTwoArrays_349 https://leetcode.com/problems/intersection-of-two-arrays/

--- src/tasks/algorithms/easy/IntersectionOfTwoArrays_349.js
+++ src/tasks/algorithms/easy/IntersectionOfTwoArrays_349.js
@@ -3,3 +3,9 @@ var intersection = function(nums1, nums2) {
     const set = new Set(nums1);
     return nums2.filter(num => set.delete(num));
 }
+
+// not my solution 56 ms beats 97%
+var intersection = function(nums1, nums2) {
+    const set = new Set(nums1);
+    return [...new Set(nums2.filter(num => set.has(num) ))];
+};
