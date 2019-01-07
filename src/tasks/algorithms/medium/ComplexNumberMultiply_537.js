3cf34b41582365377230fdd5e9fba840b6dbbccb
Ilarion <hilarion.galushka@gmail.com>
2019-01-07 23:39:59 +0200

    algorithm to solve task ComplexNumberMultiply_537

---
+++ src/tasks/algorithms/medium/ComplexNumberMultiply_537.js
@@ -0,0 +1,11 @@
+var complexNumberMultiply = function(a, b) {
+    // parse strings indexOf(+)
+    // first and second parts
+    // first2 and second2
+
+    // resultFirstpart = first * first2 - second * second2
+
+    // result second part = first * second2 + first2 * second
+
+    // return resultFirst + '+' + resultSecond + 'i'
+};
