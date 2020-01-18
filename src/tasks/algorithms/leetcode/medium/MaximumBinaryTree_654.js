d81cc6a50a71403134b9afd15fdb8dccfb7de132
IlarionHalushka <hilarion.galushka@gmail.com>
2020-01-18 11:45:26 +0200

    solve trees: MaximumBinaryTree_654.js

--- src/tasks/algorithms/leetcode/medium/MaximumBinaryTree_654.js
+++ src/tasks/algorithms/leetcode/medium/MaximumBinaryTree_654.js
@@ -1,3 +1,23 @@
+// Improved Solution using one function
+var constructMaximumBinaryTree = function(nums, low = 0, high = nums.length - 1) {
+    if (low > high) return null;
+
+    let i = low;
+
+    for (let j = low + 1; j <= high; j++) {
+        if (nums[j] > nums[i]) {
+            i = j;
+        }
+    }
+
+    const root = new TreeNode(nums[i]);
+    root.left = constructMaximumBinaryTree(nums, low, i - 1);
+    root.right = constructMaximumBinaryTree(nums, i + 1, high);
+
+    return root;
+};
+
+// With multiple functions
 var constructMaximumBinaryTree = function(nums) {
     // find max value in array and its index
     // const root = new TreeNode(maxNumber)
