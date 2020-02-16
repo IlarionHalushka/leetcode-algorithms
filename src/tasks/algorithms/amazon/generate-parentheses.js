9906f8cdb68537cb7c375d13e26c37133211256d
IlarionHalushka <hilarion.galushka@gmail.com>
2020-02-16 21:12:54 +0200

    solve amazon: generate-parentheses.js

---
+++ src/tasks/algorithms/amazon/generate-parentheses.js
@@ -0,0 +1,26 @@
+var generateParentheses = function(n) {
+  if (n === 0) {
+    return [''];
+  }
+
+  const res = [];
+  const initial = '(';
+  let left = 1;
+  let right = 0;
+
+  const recurse = (s, l, r) => {
+    if (l === n && r === n) {
+      return res.push(s);
+    }
+    if (l <= n) {
+      recurse(s + '(', l + 1, r);
+    }
+    if (r < l) {
+      recurse(s + ')', l, r + 1);
+    }
+  }
+
+  recurse(initial, left, right)
+
+  return res;
+};
