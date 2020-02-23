b3a1a54f7cf8334ebf5b6598ea3a963b5b1f7f90
IlarionHalushka <hilarion.galushka@gmail.com>
2020-02-23 20:16:26 +0200

    solve amazon: copy-list-with-random-pointer.js

--- src/tasks/algorithms/amazon/copy-list-with-random-pointer.js
+++ src/tasks/algorithms/amazon/copy-list-with-random-pointer.js
@@ -54,3 +54,27 @@ var copyRandomList = function(head) {
 
   return dummyCopy.next;
 };
+
+// BETTER SOLUTION
+// public RandomListNode copyRandomList(RandomListNode head) {
+//   if (head == null) return null;
+//
+//   Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+//
+//   // loop 1. copy all the nodes
+//   RandomListNode node = head;
+//   while (node != null) {
+//     map.put(node, new RandomListNode(node.label));
+//     node = node.next;
+//   }
+//
+//   // loop 2. assign next and random pointers
+//   node = head;
+//   while (node != null) {
+//     map.get(node).next = map.get(node.next);
+//     map.get(node).random = map.get(node.random);
+//     node = node.next;
+//   }
+//
+//   return map.get(head);
+// }
