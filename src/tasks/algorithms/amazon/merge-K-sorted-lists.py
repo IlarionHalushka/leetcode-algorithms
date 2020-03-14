6233385ba52d8a15ea43f85e883818374b64cc49
IlarionHalushka <hilarion.galushka@gmail.com>
2020-03-14 21:46:13 +0200

    solve algorithms:

---
+++ src/tasks/algorithms/amazon/merge-K-sorted-lists.py
@@ -0,0 +1,13 @@
+from Queue import PriorityQueue
+class Solution(object):
+    def mergeKLists(self, lists):
+        dummy = ListNode(None)
+        curr = dummy
+        q = PriorityQueue()
+        for node in lists:
+            if node: q.put((node.val,node))
+        while q.qsize()>0:
+            curr.next = q.get()[1]
+            curr=curr.next
+            if curr.next: q.put((curr.next.val, curr.next))
+        return dummy.next
